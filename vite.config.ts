3ee417cd590cc93c539d421bc643a36bd16b0ecc
Aleksandr Edemskii <a7edemskiy@gmail.com>
2025-09-28 23:47:26 +0300

    test4

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/sap-mismatch-validation/',
+  base: '/sap-mismatch-validation',
   plugins: [react()],
 });
