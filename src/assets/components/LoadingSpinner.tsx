c483f4c4dacaa824610cc7273b27916e533a4ce2
Aleksandr Edemskii <a7edemskiy@gmail.com>
2025-09-28 22:39:47 +0300

    remove loader text

--- src/assets/components/LoadingSpinner.tsx
+++ src/assets/components/LoadingSpinner.tsx
@@ -4,9 +4,7 @@ import './LoadingSpinner.css'
 
 const LoadingSpinner: React.FC = () => {
 	return (
-		<div className='loading-spinner'>
-			<div className='loading-spinner__text'>Drücken Sie die Eingabetaste, um fortzufahren</div>
-		</div>
+		<div className='loading-spinner'></div>
 	)
 }
 
